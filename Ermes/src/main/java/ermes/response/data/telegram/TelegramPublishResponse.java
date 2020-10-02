9fd5526e73ecf559af83182a0c8896d239a1886a
Tiziano <tizianocitro98@gmail.com>
2020-10-02 15:45:20 +0200

    Aggiunta documentazione Swagger per risposte Telegram

--- Ermes/src/main/java/ermes/response/data/telegram/TelegramPublishResponse.java
+++ Ermes/src/main/java/ermes/response/data/telegram/TelegramPublishResponse.java
@@ -1,7 +1,10 @@
 package ermes.response.data.telegram;
 
 import ermes.response.data.PublishResponse;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description="Modella la risposta per i servizi di pubblicazione per Telegram.")
 public class TelegramPublishResponse extends PublishResponse {
 	public TelegramPublishResponse() {
 
@@ -12,6 +15,12 @@ public class TelegramPublishResponse extends PublishResponse {
 		this.chatId=chatId;
 	}
 
+	
+	@Override
+	public String toString() {
+		return getClass().getName() + " [chatId=" + chatId + ", link=" + getLink() + "]";
+	}
+
 	public String getChatId() {
 		return chatId;
 	}
@@ -20,5 +29,6 @@ public class TelegramPublishResponse extends PublishResponse {
 		this.chatId=chatId;
 	}
 
+	@ApiModelProperty(notes="L'id della chat su cui si è effettuata la pubblicazione.", position=1)
 	private String chatId;
 }
